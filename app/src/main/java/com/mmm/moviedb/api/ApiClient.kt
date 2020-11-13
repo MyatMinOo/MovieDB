a76c6ad942dba50d528631df6f78021f69136ab1
MyatMinOo <myatm265@gmail.com>
2020-11-13 16:51:46 +0630

    added

---
+++ app/src/main/java/com/mmm/moviedb/api/ApiClient.kt
@@ -0,0 +1,39 @@
+package com.mmm.moviedb.api
+
+import com.mmm.moviedb.model.Movie
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApiClient {
+    private var BASE_URL = "https://api.themoviedb.org/3/movie/"
+
+   val apiInterface : ApiInterface
+
+    companion object {
+        val API_KEY = "757007c7da260a44df4cd295d28ce2db"
+    }
+
+    init {
+        val retrofit=Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        apiInterface=retrofit.create(ApiInterface::class.java)
+    }
+
+    //Top Rated
+    fun getTopRatedClient(apiKey: String,language: String,page: Int): Call<Movie> {
+        return apiInterface.getTopRated(apiKey,language,page)
+    }
+
+    //Up Coming
+    fun getUpComingClient(apiKey: String , language: String , page: Int): Call<Movie>{
+        return apiInterface.getUpcoming(apiKey,language,page)
+    }
+
+    //Detail
+    fun getDetailClient(apiKey: String, movieid: String): Call<Movie> {
+        return apiInterface.getDetail(movieid.toInt(), apiKey)
+    }
+}
---
+++ app/src/main/java/com/mmm/moviedb/api/ApiInterface.kt
@@ -0,0 +1,44 @@
+package com.mmm.moviedb.api
+
+import com.mmm.moviedb.model.Movie
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ApiInterface {
+    @GET("now_playing")
+    fun getNowPlaying(
+        @Query("api_key") api_key : String,
+        @Query("language") language : String,
+        @Query("page") page : String
+    ) : Call<Movie>
+
+    @GET("popular")
+    fun getPopular(
+        @Query("api_key") api_key : String,
+        @Query("language") language : String,
+        @Query("page") page : String
+    ) : Call<Movie>
+
+    @GET("top_rated")
+    fun getTopRated(
+        @Query("api_key") api_key : String,
+        @Query("language") language : String,
+        @Query("page") page : Int
+    ) : Call<Movie>
+
+    @GET("upcoming")
+    fun getUpcoming(
+        @Query("api_key") api_key : String,
+        @Query("language") language : String,
+        @Query("page") page : Int
+    ) : Call<Movie>
+
+    //Detail
+    @GET("{movie_id}")
+    fun getDetail(
+        @Path("movie_id") movieid : Int,
+        @Query("api_key") apiKey : String
+    ): Call<Movie>
+}
